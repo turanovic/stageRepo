7ba86acf647a258a708734834896ab3aaa7f81f2
Turan Yildiz <t.yildiz1997@gmail.com>
2017-02-06 11:38:14 +0100

    Oude code toegepast

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//store our survey answers in this array
+var surveyArray = []
+
+//submits the form onclick
+function submitForm() {
+	var form = document.getElementById('form')
+	var selected = document.getElementById('select').value
+	var checkboxes = form.elements.checkbox
+	var radios = form.elements.radio
+	var checkboxesArray = []
+	
+
+	for(i = 0; i < checkboxes.length; i++) {
+		if(checkboxes[i].checked)
+			checkboxesArray.push(checkboxes[i].value)
+	}
+
+	for(i = 0; i < radios.length; i++) {
+		if(radios[i].checked)
+			var radioValue = radios[i].value
+	}
+
+	var newSurvey = {
+		checked: checkboxesArray,
+		selected: selected,
+		radiod: radioValue
+	}
+
+	console.log(checkboxesArray)
+	console.log(selected)
+	console.log(radioValue)
+
+	surveyArray.push(newSurvey)
+
+	console.log(surveyArray)
+}
+var display = document.getElementById('submitButton')
+display.addEventListener('click', submitForm, false)
