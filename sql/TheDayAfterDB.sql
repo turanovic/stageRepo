c1e06f0c127a4369050b784a491f55c43071c69e
maikel voogd <maikel.voogd@edu-kw1c.nl>
2017-02-06 13:55:42 +0100

    database bestanden toegevoegd

---
+++ sql/TheDayAfterDB.sql
@@ -0,0 +1,21 @@
+create database TheDayAfterDB;
+
+create table user
+(
+	userID			int,
+    age				int,
+    gender			varchar(10),
+    education		varchar(30),
+    residence		varchar(30),
+    nationality		varchar(30)
+);
+
+create table answers
+(
+	
+);
+
+create table email
+(
+
+);
