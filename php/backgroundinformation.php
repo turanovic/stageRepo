dabfe53a892c94ca541d628160dd9e2fec2851a8
Dylan Scheffers <dylan.scheffers@edu-kw1c.nl>
2017-02-06 13:29:43 +0100

    syntax error verholpen

--- php/backgroundinformation.php
+++ php/backgroundinformation.php
@@ -37,7 +37,7 @@
                 <select>
                     <?php
                         for($i = 16; $i = < 100; $i++){
-                            echo'<option value="' . $i . '">' . $i . '</option>'
+                            echo'<option value="' . $i . '">' . $i . '</option>';
                         }
                     ?>
 
@@ -45,61 +45,30 @@
 
 
 
-                <asp:DropDownList ID="ddlLeeftijd" class="form-control" runat="server"></asp:DropDownList>
-
+                
                 <br />
                 <!-- question 2 using dropdownlist -->
                 <h3>Wat is je geslacht?</h3>
-                <asp:DropDownList ID="ddlGeslacht" class="form-control" runat="server"></asp:DropDownList>
+
 
                 <br />
                 <!-- question 3 using select and options -->
                 <h3>Welke opleiding volg je?</h3>
-                <asp:DropDownList ID="ddlOpleiding" class="form-control" runat="server">
-                    <asp:ListItem>Geen</asp:ListItem>
-                    <asp:ListItem>Administratief / Secretarieel</asp:ListItem>
-                    <asp:ListItem>Design / AV-produktie / Theater</asp:ListItem>
-                    <asp:ListItem>Bouw / Hout / Afwerkingstechniek</asp:ListItem>
-                    <asp:ListItem>Elektro- / Installatie- / Proces- / Koudetechniek / Metaal</asp:ListItem>
-                    <asp:ListItem>Economie</asp:ListItem>
-                    <asp:ListItem>Handel/Verkoop</asp:ListItem>
-                    <asp:ListItem>Entree Breed</asp:ListItem>
-                    <asp:ListItem>Horeca / Toerisme</asp:ListItem>
-                    <asp:ListItem>ICT</asp:ListItem>
-                    <asp:ListItem>Juridisch</asp:ListItem>
-                    <asp:ListItem>Sport</asp:ListItem>
-                    <asp:ListItem>Techniek</asp:ListItem>
-                    <asp:ListItem>Veiligheid / Veiligheid &amp; Techniek / Handhaving</asp:ListItem>
-                    <asp:ListItem>Voertuigen / Transport en Logistiek </asp:ListItem>
-                    <asp:ListItem>VAVO</asp:ListItem>
-                    <asp:ListItem>Zorg &amp; Welzijn</asp:ListItem>
-                </asp:DropDownList>
+
                 <br />
                 <!-- question 4 using dropdownlist -->
                 <h3>In welke provincie woon je?</h3>
-                <asp:DropDownList ID="ddlProvincie" class="form-control" runat="server"></asp:DropDownList>
+
   
                 <br />
                 <!-- question 5 using dropdownlist -->
                 <h3>Wat is je nationaliteit?</h3>
-                <asp:DropDownList ID="ddlNationaliteit" class="form-control" runat="server">
-                    <asp:ListItem>Nederlands</asp:ListItem>
-                    <asp:ListItem>Pools</asp:ListItem>
-                    <asp:ListItem>Turkse</asp:ListItem>
-                    <asp:ListItem>Duits</asp:ListItem>
-                    <asp:ListItem>Brits</asp:ListItem>
-                    <asp:ListItem>Marokkaans</asp:ListItem>
-                    <asp:ListItem>Belgisch</asp:ListItem>
-                    <asp:ListItem>Chinees</asp:ListItem>
-                    <asp:ListItem>Italiaans</asp:ListItem>
-                    <asp:ListItem>Spaans</asp:ListItem>
-                    <asp:ListItem>Syrisch</asp:ListItem>
-                </asp:DropDownList> 
+
                 <br />
 
                 <br />
                 <div class="form-actions">
-        <asp:Button ID="Button1" runat="server" Text="Volgende" onClick="Button1_Click"/>
+
 
                 </div>
 
