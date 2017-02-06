1e7349d99966c64ec0fba372e52ea02d9b94fcbf
maikel voogd <maikel.voogd@edu-kw1c.nl>
2017-02-06 12:58:43 +0100

    index.php/database.php

---
+++ php/database.php
@@ -0,0 +1,29 @@
+<?php
+
+class database
+{
+
+  //variables
+  public $host;
+  public $user;
+
+  //database function
+  function db()
+  {
+    $this->host="localhost";
+
+  }
+
+  //connect funtion
+  public function connect()
+  {
+
+  }
+
+  //connect function
+  public function insert()
+  {
+
+  }
+}
+?>
