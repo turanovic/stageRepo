162c5c34cf1fb06cedc0439e254651408ad0700d
Dylan Scheffers <dylan.scheffers@edu-kw1c.nl>
2017-02-06 13:42:48 +0100

    Opleiding en geslacht toegevoegt. Afbeeldingen toegevoegt

--- include/banner.inc.php
+++ include/banner.inc.php
@@ -7,10 +7,10 @@
 <div id="background"></div>
 
 <nav class="navbar navbar-default">
-  <img src="Images/kw1c.png" alt="Mountain View" class="pull-right" style="width:64px;height:68px;">
+  <img src="../images/kw1c.png" alt="Mountain View" class="pull-right" style="width:64px;height:68px;">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
-      <img src="Images/synerscope.png" alt="Mountain View" class="pull-left "style="width:94px;height:68px;">
+      <img src="../images/synerscope.png" alt="Mountain View" class="pull-left "style="width:94px;height:68px;">
       <div class="navbar-header" style="width:80%;">
         <div class="h1" style="text-align:center; font-family:sans-serif;">
             Vragenlijst
--- php/backgroundinformation.php
+++ php/backgroundinformation.php
@@ -35,6 +35,8 @@
                 <!-- question 1 using dropdownlist -->
                 <h3>Wat is je leeftijd?</h3>
                 <select>
+
+                    <!-- php for loop for inserting ages in dropdownlist -->
                     <?php
                         for($i = 16; $i < 100; $i++){
                             echo'<option value="' . $i . '">' . $i . '</option>';
@@ -42,18 +44,38 @@
                     ?>
 
                 </select>
-
-
-
                 
                 <br />
                 <!-- question 2 using dropdownlist -->
                 <h3>Wat is je geslacht?</h3>
+                <select>
+                    <option value="Vrouw">Vrouw</option>
+                    <option value="Man">Man</option>
+                </select>
 
 
                 <br />
                 <!-- question 3 using select and options -->
                 <h3>Welke opleiding volg je?</h3>
+                <select>
+                    <option value="Geen">Geen</option>
+                    <option value="Administratief / Secretarieel">Administratief / Secretarieel</option>
+                    <option value="Design / AV-produktie / Theater">Design / AV-produktie / Theater</option>
+                    <option value="Bouw / Hout / Afwerkingstechniek">Bouw / Hout / Afwerkingstechniek</option>
+                    <option value="Elektro- / Installatie- / Proces- / Koudetechniek / Metaal">Elektro- / Installatie- / Proces- / Koudetechniek / Metaal</option>
+                    <option value="Economie">Economie</option>
+                    <option value="Handel/Verkoop">Handel/Verkoop</option>
+                    <option value="Entree Breed">Entree Breed</option>
+                    <option value="Horeca / Toerisme">Horeca / Toerisme</option>
+                    <option value="ICT">ICT</option>
+                    <option value="Juridisch">Juridisch</option>
+                    <option value="Sport">Sport</option>
+                    <option value="Techniek">Techniek</option>
+                    <option value="Veiligheid / Veiligheid & Techniek / Handhaving">Veiligheid / Veiligheid & Techniek / Handhaving</option>
+                    <option value="Voertuigen / Transport en Logistiek">Voertuigen / Transport en Logistiek</option>
+                    <option value="VAVO">VAVO</option>
+                    <option value="Zorg & Welzijn">Zorg & Welzijn</option>
+                </select>
 
                 <br />
                 <!-- question 4 using dropdownlist -->
