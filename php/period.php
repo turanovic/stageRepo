b85c3da04141d5c9c3864f6b78de816baf73e540
maikel voogd <maikelv@live.nl>
2017-02-14 10:48:44 +0100

    periode functie

--- php/period.php
+++ php/period.php
@@ -1,36 +1,50 @@
 <?php
 
-$period;
-$start1 = "2017-02-13 00:00:00";
-$start2 = "2017-02-20 00:00:00";
-$start3 = "2017-02-27 00:00:00";
-$start4 = "2017-03-06 00:00:00";
-$end1 = "2017-02-19 23:59:59";
-$end2 = "2017-02-26 23:59:59";
-$end3 = "2017-03-05 23:59:59";
-$end4 = "2017-03-12 23:59:59";
-
-function getPeriod()
+class period
 {
-  if(strtotime('now') >= strtotime($start1) && strtotime('now') <= strtotime($end1))
+  private $period;
+  private $start1 = "2017-02-20 00:00:00";
+  private $start2 = "2017-02-27 00:00:00";
+  private $start3 = "2017-03-06 00:00:00";
+  private $start4 = "2017-03-13 00:00:00";
+  private $end1 = "2017-02-26 23:59:59";
+  private $end2 = "2017-03-05 23:59:59";
+  private $end3 = "2017-03-12 23:59:59";
+  private $end4 = "2017-03-19 23:59:59";
+
+  public function getPeriod()
   {
-    $period = 1;
+    return $this->period;
   }
 
-  else if(strtotime('now') >= strtotime($start2) && strtotime('now') <= strtotime($end2))
+  public function setPeriod($period)
   {
-    $period = 2;
+    $this->period = $period;
   }
 
-  else if (strtotime('now') >= strtotime($start3) && strtotime('now') <= strtotime($end3))
+  function definePeriod()
   {
-    $period = 3;
+    if(strtotime('now') >= strtotime($start1) && strtotime('now') <= strtotime($end1))
+    {
+      $period = 1;
+    }
+
+    else if(strtotime('now') >= strtotime($start2) && strtotime('now') <= strtotime($end2))
+    {
+      $period = 2;
+    }
+
+    else if (strtotime('now') >= strtotime($start3) && strtotime('now') <= strtotime($end3))
+    {
+      $period = 3;
+    }
+
+    else if (strtotime('now') >= strtotime($start4) && strtotime('now') <= strtotime($end4))
+    {
+      $period = 4;
+    }
   }
 
-  else if (strtotime('now') >= strtotime($start4) && strtotime('now') <= strtotime($end4))
-  {
-    $period = 4;
-  }
 }
 
  ?>
